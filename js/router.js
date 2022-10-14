b8052913741f7a172462da726fa5e1106b5d1339
vdi-terminal <vdi-terminal@EDU-WSB.local>
2022-10-14 11:06:53 +0200

    first commit

--- js/router.js
+++ js/router.js
@@ -24,19 +24,20 @@ let pageUrls = {
    });
    function RenderAboutPage(params) {
     document.querySelector('main').innerHTML =
-    `<h1 class="title">About Me</h1>
-    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-   Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-   printer took a galley of type and scrambled it to make a type specimen book. It has survived
-   not only five centuries, but also the leap into electronic typesetting, remaining essentially
-   unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-   Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-   PageMaker including versions of Lorem Ipsum.</p>`;
+    `<h1 class="title">Our Mission</h1>`;
    }
    function RenderfaqPage(params) {
     document.querySelector('main').innerHTML =
-    `<h1 class="title">FrequentlyAskedQuestions</h1>
+    `<h1 class="title">Frequently Asked Questions</h1>
     <p>Question: How much does it cost?</p></b><p>Answear: Go to pricing</p>`;
+   }
+   function RenderPricingPage(params) {
+    document.querySelector('main').innerHTML =
+    `<h1 class="title">Pricing</h1>`;
+   }
+   function RenderSignUpPage(params) {
+    document.querySelector('main').innerHTML =
+    `<h1 class="title">Sing Up Now!</h1>`;
    }
    function popStateHandler(event) {
     loc = window.location.href.toString().split(window.location.host)[1];
