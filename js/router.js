306e8eaf6f967125ab68d84676a85324dc9facc9
Malwina <yellowindmills@gmail.com>
2022-12-12 14:49:18 +0100

    More fixes

--- js/router.js
+++ js/router.js
@@ -43,6 +43,9 @@ let pageUrls = {
    function RenderAboutPage(params) {
     document.querySelector('main').innerHTML =
     `<h1 class="title">Our Mission</h1>
+    <h2>Improving lives through learning</h2>
+    <p>Whether you want to learn or to share what you know, you’ve come to the right place. As a global destination for online learning, we connect people through knowledge.
+    We help organizations of all types and sizes prepare for the path ahead — wherever it leads. Our curated collection of business and technical courses help companies, governments, and nonprofits go further by placing learning at the center of their strategies.</p>
     `;
    }
    function RenderfaqPage(params) {
